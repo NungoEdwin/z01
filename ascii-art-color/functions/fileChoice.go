759672849d83facfd508e9d7b07183cb47927d2e
nungo <nued65@gmail.com>
2024-07-18 07:48:48 +0300

    fix:handle increased number of args with new intoduction of flag and banner option for printing

--- ascii-art-color/functions/fileChoice.go
+++ ascii-art-color/functions/fileChoice.go
@@ -12,7 +12,19 @@ func FileChoice(osArgs []string) []string {
 
 	if len(os.Args) == 3 && !(strings.HasPrefix(os.Args[1], "--color")) {
 		banner = osArgs[2] + ".txt"
+	} else if len(os.Args) == 4 && strings.HasPrefix(os.Args[1], "--color") {
+		if os.Args[3] == "shadow" || os.Args[3] == "thinkertoy" || os.Args[3] == "standard" {
+			banner = os.Args[3] + ".txt"
+		}
+	} else if len(os.Args) == 5 && strings.HasPrefix(os.Args[1], "--color") {
+		if os.Args[4] == "shadow" || os.Args[4] == "thinkertoy" || os.Args[4] == "standard" {
+			banner = os.Args[4] + ".txt"
+		} else {
+			fmt.Println("Usage: go run . [OPTION] [STRING]\n\nEX: go run . --color=<color> <substring to be colored> \"something\"")
+			return []string{}
+		}
 	}
+
 	file, err := os.ReadFile(banner)
 	if err != nil {
 		fmt.Println(err)
