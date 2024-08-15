846bae92c09890e909994660c611569c5a99e2ba
nungo <nued65@gmail.com>
2024-08-15 22:33:57 +0300

    feat:allowing only flag with -- format and refusing single - or any other formats

--- ascii-art-output/functions/userInputString.go
+++ ascii-art-output/functions/userInputString.go
@@ -9,13 +9,6 @@ import (
 
 // InputArgs function processes command-line arguments
 func InputArgs(osArgs []string) (string, string, int) {
-	/*//Alternative way using flag.StringVar
-	var color string
-	var output string
-	flag.StringVar(&color,"color"."\033[0m","Provide color code for coloring.")
-	flag.SringVar(&output,"output","banner.txt","Provide the output banner file")
-	flag.Parse()
-	*/
 	var (
 		color  = flag.String("color", "\033[0m", "Provide color code to be used for coloring.")
 		output = flag.String("output", "banner.txt", "Provide the output file")
@@ -41,8 +34,16 @@ func InputArgs(osArgs []string) (string, string, int) {
 
 	flag.Visit(func(f *flag.Flag) {
 		if f.Name == "color" {
+			if !flagPattern(osArgs, "--color") {
+				fmt.Println("Usage: go run . [OPTION] [STRING]\n\nEX: go run . --color=<color> <substring to be colored> something")
+				os.Exit(0)
+			}
 			flagSet = true
 		} else if f.Name == "output" {
+			if !flagPattern(osArgs, "--output") {
+				fmt.Println("Usage: go run . [OPTION] [STRING] [BANNER]\n\nEX: go run . --output=<fileName.txt> something standard")
+				os.Exit(0)
+			}
 			flagset2 = true
 		} else {
 			fmt.Println("Usage: go run . [OPTION] [STRING] [BANNER]\n\nEX: go run . --output=<fileName.txt> something standard")
@@ -95,4 +96,13 @@ func InputArgs(osArgs []string) (string, string, int) {
 	args = strings.ReplaceAll(args, "\\t", " ")
 
 	return args, match, len(osArgs)
+}
+
+func flagPattern(s []string, flagord string) bool {
+	for _, arg := range s {
+		if strings.HasPrefix(arg, flagord) {
+			return true
+		}
+	}
+	return false
 }
