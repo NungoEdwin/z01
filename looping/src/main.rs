839687b3b9f74c0d9417342af9619d510bb8ed8e
nungo <nued65@gmail.com>
2025-04-02 13:24:12 +0300

    fix:looping

--- looping/src/main.rs
+++ looping/src/main.rs
@@ -1,14 +1,15 @@
 use std::io;
 fn main() {
+    let mut gotcha=false
     let mut input=String::new();
     let mut c=0;
-    loop{
+    while !gotcha{
         println!("I am the beginning of the end, and the end of time and space. I am essential to creation, and I surround every place. What am I?");
         let _=io::stdin().read_line(&mut input);
         c+=1;
         if input=="The letter e"{
           println!("Number of trials {}",c);
-          break;
+          gotcha=true;
         }
     }
 }
