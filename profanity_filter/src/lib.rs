3e416275a48aad774c0b7b90719d444be25695ee
Christopher Fremond <dev@01-edu.org>
2025-04-09 22:40:23 +0000

    Update profanity_filter/src/lib.rs

--- profanity_filter/src/lib.rs
+++ profanity_filter/src/lib.rs
@@ -5,21 +5,23 @@ pub struct Message {
 
 impl Message {
     pub fn new(content: String, user: String) -> Self {
-        Message { content, user }
+        Self { content, user }
     }
 
-    pub fn send_ms(&self) -> Option<&str> {
+    pub fn send_ms(self) -> Option<String> {
         if self.content.is_empty() || self.content.contains("stupid") {
             None
         } else {
-            Some(&self.content)
+            Some(self.content)
         }
     }
 }
 
-pub fn check_ms(message: &Message) -> Result<String, String> {
-    match message.send_ms() {
-        Some(content) => Ok(content.to_string()),
-        None => Err("ERROR: illegal".to_string()),
+pub fn check_ms(message: &str) -> Result<&str, &str> {
+    let msg = Message::new(message.to_string(), "anonymous".to_string());
+
+    match msg.send_ms() {
+        Some(content) => Ok(Box::leak(content.into_boxed_str())),
+        None => Err("ERROR: illegal"),
     }
 }
