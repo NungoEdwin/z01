2def1284def11bf75080338f5a6a63877615549b
Nungo <nued65@gmail.com>
2025-04-12 06:40:34 +0300

    fix:ordinal

--- ordinal/src/lib.rs
+++ ordinal/src/lib.rs
@@ -1,5 +1,6 @@
 pub fn num_to_ordinal(x: u32) -> String {
     let mut a :String=x.to_string();
+if !(x>10&&x<20){
    if x%10==1{
        a.push_str("st");
    }else if x%10==2{
@@ -9,6 +10,9 @@ pub fn num_to_ordinal(x: u32) -> String {
    }else{
        a.push_str("th");
    }
+}else{
+    a.push_str("th")
+}
    a
 
 }
