fb838f284fcd53a527a5e1780e7ce90fc79f7ba5
nungo <nued65@gmail.com>
2025-04-27 01:40:56 +0300

    add:romsn_numbers

--- roman_numbers/src/lib.rs
+++ roman_numbers/src/lib.rs
@@ -1,3 +1,6 @@
+mod iterators;
+use crate::RomanDigit::*;
+
 #[derive(Copy, Clone, Debug, PartialEq, Eq)]
 pub enum RomanDigit {
     Nulla,
@@ -10,81 +13,58 @@ pub enum RomanDigit {
     M,
 }
 
-#[derive(Debug, Clone, PartialEq, Eq)]
-pub struct RomanNumber(pub Vec<RomanDigit>);
-
 impl From<u32> for RomanDigit {
-    fn from(num: u32) -> Self {
-        match num {
-            1 => RomanDigit::I,
-            5 => RomanDigit::V,
-            10 => RomanDigit::X,
-            50 => RomanDigit::L,
-            100 => RomanDigit::C,
-            500 => RomanDigit::D,
-            1000 => RomanDigit::M,
-            _ => RomanDigit::Nulla,
+    fn from(n: u32) -> Self {
+        match n {
+            1..=4 => I,
+            5..=9 => V,
+            10..=49 => X,
+            50..=99 => L,
+            100..=499 => C,
+            500..=999 => D,
+            1000..=5000 => M,
+            _ => Nulla,
         }
     }
 }
 
+#[derive(Debug)]
+pub struct RomanNumber(pub Vec<RomanDigit>);
+
 impl From<u32> for RomanNumber {
-    fn from(mut num: u32) -> Self {
-        if num == 0 {
-            return RomanNumber(vec![RomanDigit::Nulla]);
+    fn from(n: u32) -> Self {
+        if n == 0 {
+            return RomanNumber(vec![Nulla]);
         }
 
-        let mut digits = Vec::new();
+        let mut quotient = n;
+        let mut p = 0;
+        let mut reverse_roman = Vec::new();
 
-        while num > 0 {
-            if num >= 1000 {
-                digits.push(RomanDigit::M);
-                num -= 1000;
-            } else if num >= 900 {
-                digits.push(RomanDigit::C);
-                digits.push(RomanDigit::M);
-                num -= 900;
-            } else if num >= 500 {
-                digits.push(RomanDigit::D);
-                num -= 500;
-            } else if num >= 400 {
-                digits.push(RomanDigit::C);
-                digits.push(RomanDigit::D);
-                num -= 400;
-            } else if num >= 100 {
-                digits.push(RomanDigit::C);
-                num -= 100;
-            } else if num >= 90 {
-                digits.push(RomanDigit::X);
-                digits.push(RomanDigit::C);
-                num -= 90;
-            } else if num >= 50 {
-                digits.push(RomanDigit::L);
-                num -= 50;
-            } else if num >= 40 {
-                digits.push(RomanDigit::X);
-                digits.push(RomanDigit::L);
-                num -= 40;
-            } else if num >= 10 {
-                digits.push(RomanDigit::X);
-                num -= 10;
-            } else if num == 9 {
-                digits.push(RomanDigit::I);
-                digits.push(RomanDigit::X);
-                num -= 9;
-            } else if num >= 5 {
-                digits.push(RomanDigit::V);
-                num -= 5;
-            } else if num == 4 {
-                digits.push(RomanDigit::I);
-                digits.push(RomanDigit::V);
-                num -= 4;
+        while quotient != 0 {
+            let rest = quotient % 10;
+            quotient /= 10;
+            p += 1;
+            if rest == 9 {
+                reverse_roman.push(RomanDigit::from(10_u32.pow(p)));
+                reverse_roman.push(RomanDigit::from(10_u32.pow(p - 1)));
+            } else if rest == 4 {
+                reverse_roman.push(RomanDigit::from(10_u32.pow(p) / 2));
+                reverse_roman.push(RomanDigit::from(10_u32.pow(p - 1)));
+            } else if rest >= 5 {
+                let repetitions = rest - 5;
+                for _ in 0..repetitions {
+                    reverse_roman.push(RomanDigit::from(10_u32.pow(p - 1)));
+                }
+                reverse_roman.push(RomanDigit::from(10_u32.pow(p) / 2));
             } else {
-                digits.push(RomanDigit::I);
-                num -= 1;
+                for _ in 0..rest {
+                    reverse_roman.push(RomanDigit::from(10_u32.pow(p - 1)))
+                }
             }
         }
 
-        RomanNumber(digits)
+        reverse_roman.reverse();
+        RomanNumber(reverse_roman)
     }
 }
