21fc56b3101f9698410d5f6a732d1e2c150a0660
nungo <nued65@gmail.com>
2025-11-03 14:16:30 +0300

    fix null on the other variable case

--- StringConcat/StringConcat.java
+++ StringConcat/StringConcat.java
@@ -2,7 +2,8 @@ public class StringConcat{
 public static String concat(String s1,String s2){
  if (s1==null){
  return s2;
- }
+ }else if(s2==null){
+ return s1;}
  return s1 + s2;
 }
 public static void main(String args[]){
