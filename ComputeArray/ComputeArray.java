311cff7c1cb1bef533296c0656548ca24184ead4
Nungo <nued65@gmail.com>
2025-11-05 07:15:41 +0300

    fix ComputeArray

--- ComputeArray/ComputeArray.java
+++ ComputeArray/ComputeArray.java
@@ -6,28 +6,25 @@ public class ComputeArray {
         int[] result = new int[array.length];
         for (int i = 0; i < array.length; i++) {
             int x = array[i];
-            int mod3 = x % 3;
-            if (mod3 < 0) {
-                mod3 += 3;
-            }
-            if (mod3 == 0) {
-                result[i] = x * 5;
-            } else if (mod3 == 1) {
+            int rem = x % 3;  // remainder can be 0, 1, 2, or negative (‐1, ‐2) for negative x
+
+            if (rem == 0) {
+              result[i] = x * 5;
+                
+            } else if (rem == 1) {
                 result[i] = x + 7;
-            } else {
-                // mod3 == 2
+            }else if (rem==2){
                 result[i] = x;
             }
         }
         return result;
     }
 
-
       public static void main(String[] args) {
-        int[] array = ComputeArray.computeArray(new int[]{9, 13, 8, 23, 1, 0, 89});
+        int[] array = ComputeArray.computeArray(new int[]{20, 65, 30, -67, 75, 11, -1, -30, 17});
         for (int i : array) {
             System.out.print(i + " ");
         }
-        System.out.println("\n");
+        System.out.println();
     }
 }
