af5924740d1e3513b2df22e374039176175e58a8
nungo <nued65@gmail.com>
2025-11-05 13:23:40 +0300

    add:DoOp

---
+++ DoOp/DoOp.java
@@ -0,0 +1,45 @@
+public class DoOp{
+public static String operate(String[] args){
+	if(args.length!=3){
+	return "it depend on the input.";
+	}
+		try{	
+		int left=Integer.parseInt(args[0]);
+		int right=Integer.parseInt(args[2]);
+		String sign=args[1];
+		int tot=0;
+	switch (sign){
+			case "+":{
+			tot=left + right;
+			break;
+			}
+			case "-":{
+			tot=left - right;
+			break;
+			}
+			case "x":{
+			tot=left * right;
+			break;
+			}
+			case "/":{
+			tot=left / right;
+			break;
+			}
+			case "%":{
+			tot= left % right;
+			break;
+			}			 
+	}
+	return Integer.toString(tot);
+}catch(ArithmeticException e){
+	return "Error";
+}
+}
+public static void main(String args[]){
+ 		System.out.println(DoOp.operate(new String[]{"1","+","2"}));
+        System.out.println(DoOp.operate(new String[]{"1","-","1"}));
+        System.out.println(DoOp.operate(new String[]{"1","%","0"}));
+        System.out.println(DoOp.operate(args));
+}
+
+}
