f6002abf093a9ac03ed083c19a6e031143b66bb5
Nungo <nued65@gmail.com>
2025-11-17 17:27:50 +0300

    fix

--- ListEquals/ListEquals.java
+++ ListEquals/ListEquals.java
@@ -3,9 +3,9 @@ public class ListEquals{
 public static boolean areListsEqual(List<String> list1,List<String> list2){
 
 if(list1==null&&list2==null){
-return false;
-}else if((list1==null|| list2==null) && (list1!=null||list2!=null)){
 return true;
+}else if((list1==null|| list2==null) && (list1!=null||list2!=null)){
+return false;
 }
 return list1.equals(list2);
 
