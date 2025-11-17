bd53bf9a1b54aa18cb576604a055495128d0ef0e
Nungo <nued65@gmail.com>
2025-11-17 16:29:13 +0300

    fix using safer .equal method fir Integers is safer as compares not only the references

--- ListContains/ListContains.java
+++ ListContains/ListContains.java
@@ -2,12 +2,14 @@ import java.util.List;
 public class ListContains{
 public static boolean containsValue(List<Integer>list,Integer value){
 	for(Integer val:list){
-	if(val==value)return true;	
+	if(val.equals(value)){
+		return true;
+	}	
 	}
 	return false;
 }
   public static void main(String[] args) {
-        System.out.println(ListContains.containsValue(List.of(9, 13, 8, 23, 1, 0, 89), 8));
+        System.out.println(ListContains.containsValue(List.of(28, 438, 54857, 4374, 4893, 48394), 4893));
         System.out.println(ListContains.containsValue(List.of(9, 13, 8, 23, 1, 0, 89), 10));
     }
 }
