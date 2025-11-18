7124555dabb2144be2fadb2948e88dbf85644bf7
nungo <nued65@gmail.com>
2025-11-18 17:37:01 +0300

    add cont..

---
+++ StreamCollect/StreamCollect.java
@@ -0,0 +1,18 @@
+public class StreamCollect {
+    public static Map<Character, List<String>> mapByFirstLetter(Stream<String> s) {
+        // your code here
+    }
+
+    public static Map<Integer, Optional<Integer>> getMaxByModulo4(Stream<Integer> s) {
+        // your code here
+    }
+
+    public static String orderAndConcatWithSharp(Stream<String> s) {
+        // your code here
+    }
+public static void main(String[] args) {
+        System.out.println(StreamCollect.mapByFirstLetter(Stream.of("Bonjour", "le", "monde !", "bonsoir")));
+        System.out.println(StreamCollect.getMaxByModulo4(Stream.of(5, 12, 32, 4, 9, 17, 98, 424, 97, 5843, 48354)));
+        System.out.println(StreamCollect.orderAndConcatWithSharp(Stream.of("Hello", "how are you ?", "where do you live ?", "Bordeaux")));
+    }
+}
