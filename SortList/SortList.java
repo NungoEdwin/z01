395c84a8cde05090b4f8db79b273b8df5cf6fd76
nungo <nued65@gmail.com>
2025-11-18 13:27:06 +0300

    fix empty case

--- SortList/SortList.java
+++ SortList/SortList.java
@@ -3,7 +3,10 @@ import java.util.List;
 public class SortList {
 
     public static List<Integer> sort(List<Integer> list) {
-        if(list==null||list.isEmpty()) return new ArrayList<Integer>();
+        if(list==null||list.isEmpty()) {
+            if(list.isEmpty()) return list;
+            return new ArrayList<Integer>();
+        }
   List<Integer> copy=new ArrayList(list);
         copy.sort((a,b) -> {return a.compareTo(b);});
         //copy.sort(Integer::compareTo);
@@ -11,7 +14,10 @@ public class SortList {
    	}
 
     public static List<Integer> sortReverse(List<Integer> list) {
-    if(list==null|| list.isEmpty()) return new ArrayList<Integer>();
+    if(list==null||list.isEmpty()) {
+            if(list.isEmpty()) return list;
+            return new ArrayList<Integer>();
+        }
     List<Integer> copy=new ArrayList(list);
    copy.sort( (a,b) -> {return -1 * a.compareTo(b);});
 return copy;
