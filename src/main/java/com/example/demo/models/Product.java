f0c093c9b71d1e2f062fa77f97c09284db87c18f
nungo <nued65@gmail.com>
2026-01-14 17:18:49 +0300

    add:basic models for users and product

--- src/main/java/com/example/demo/models/Product.java
+++ src/main/java/com/example/demo/models/Product.java
@@ -4,13 +4,19 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Data;
 
 @Entity
+@Data
 public class Product {
     @Id
    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private String id;
 
+    private String name;
+    private String description;
+    private Double price;
+    private String userId;
 
 
 }
