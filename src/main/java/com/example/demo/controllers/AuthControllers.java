87195e19ffa9479e9170bcb06dd0fb1a8f6fb341
Nungo <nued65@gmail.com>
2026-01-19 16:56:37 +0300

    add:wirerd up controllers with their respective services(the products)

--- src/main/java/com/example/demo/controllers/AuthControllers.java
+++ src/main/java/com/example/demo/controllers/AuthControllers.java
@@ -1,28 +1,39 @@
 package com.example.demo.controllers;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
-import com.example.demo.controllers.Controllers.Product;
+import com.example.demo.models.Product;
+import com.example.demo.services.ProductService;
 
+@RestController
 public class AuthControllers {
+    @Autowired
+    ProductService productService;
     @PostMapping("/products")
-    public Product CreateProduct(){
-    return new Product();
+    public List<Product> CreateProduct(){
+    return productService.getAll();
     }
     @PutMapping("/products/{id}")
-    public Product UpdateProduct(){
-        return new Product();
+    public Product UpdateProduct(@RequestBody Product entity,@PathVariable String id){
+        return productService.update(entity, id);
     }
     @DeleteMapping("/products/{id}")
-    public Product DeleteProduct(){
-        return new Product();
+    public void DeleteProduct(@PathVariable String id){
+         productService.deleteProduct(id);
     }
-    @GetMapping
-    public Product GetUsers(){
-        return new Product();
+    @GetMapping("/products/{id}")
+    public Optional<Product> GetProduct(@PathVariable String id){
+        return productService.getProduct(id);
     }
     
 }
--- src/main/java/com/example/demo/services/ProductService.java
+++ src/main/java/com/example/demo/services/ProductService.java
@@ -20,7 +20,8 @@ public class ProductService {
         return repository.findById(id);
     }
     public void deleteProduct(String id){
-       Optional<Product> pd=repository.findById(id);
+       Optional<Product> pd=repository.findById(id);// Product pd = repository.findById(id)
+            //.orElseThrow(() -> new RuntimeException("Product not found"));
        pd.ifPresent(repository::delete); 
         //Alternalively
        //repository.deleteById(id);
@@ -28,6 +29,16 @@ public class ProductService {
     public void createProduct(Product entity){
         repository.save(entity);
 
+    }
+    public Product update(Product updatedProduct,String id){
+
+        Product product = repository.findById(id).orElse(null);
+        if (product!=null){
+            product.setName(updatedProduct.getName());
+            product.setPrice(updatedProduct.getPrice());
+            return repository.save(product);
+        }
+        return null;
     }
 
 }
