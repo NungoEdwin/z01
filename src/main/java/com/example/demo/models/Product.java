c9210bee94b02c29a8a7025d7e6ef7cd10d25b5b
Nungo <nued65@gmail.com>
2026-01-20 10:37:09 +0300

    fix:product table not being created as string doest use generation type of identity and rather uuid

--- src/main/java/com/example/demo/models/Product.java
+++ src/main/java/com/example/demo/models/Product.java
@@ -14,7 +14,7 @@ import lombok.NoArgsConstructor;
 @Data
 public class Product {
     @Id
-   @GeneratedValue(strategy = GenerationType.IDENTITY)
+   @GeneratedValue(strategy = GenerationType.UUID)
     private String id;
 
     private String name;
