d2de4303cd62b50fd9e06d0cd64e00ee17ccfae4
Nungo <nued65@gmail.com>
2026-01-22 16:01:45 +0300

    fix:add routing error handling

---
+++ src/main/java/com/example/demo/error/ApiErrorController.java
@@ -0,0 +1,27 @@
+package com.example.demo.error;
+
+import java.net.URI;
+
+import org.springframework.boot.webmvc.error.ErrorController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.http.HttpServletRequest;
+@RestController
+public class ApiErrorController implements ErrorController {
+    
+    @RequestMapping("/error")
+    public ProblemDetail handleError(HttpServletRequest request){
+         int status = (int) request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+
+        ProblemDetail problem = ProblemDetail.forStatus(status);
+        problem.setTitle(HttpStatus.valueOf(status).getReasonPhrase());
+        problem.setDetail("Endpoint does not exist or method not allowed");
+        problem.setInstance(URI.create(request.getRequestURI()));
+
+        return problem;
+    }
+}
--- src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
@@ -10,15 +10,15 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
   
-    @ExceptionHandler(Exception.class)
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public Map<String, Object> handleGeneral(Exception ex) {
-        return Map.of(
-            "status",HttpStatus.INTERNAL_SERVER_ERROR,
-            "error", "Internal Server Error",
-            "message", ex.getMessage()
-        );
-    }
+    // @ExceptionHandler(Exception.class)
+    // @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    // public Map<String, Object> handleGeneral(Exception ex) {
+    //     return Map.of(
+    //         "status",HttpStatus.INTERNAL_SERVER_ERROR,
+    //         "error", "Internal Server Error",
+    //         "message", ex.getMessage()
+    //     );
+    // }
 
 
 }
