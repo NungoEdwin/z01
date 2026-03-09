37dea30cdc037482ae4537fd8017cacfe840464f
nungo <nued65@gmail.com>
2026-03-09 16:08:46 +0300

    feat: add user and user-role models

---
+++ user-service/src/main/java/com/ecommerce/user/model/User.java
@@ -0,0 +1,41 @@
+package com.ecommerce.user.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collection = "users")
+public class User {
+    
+    @Id
+    private String id;
+    
+    @Indexed(unique = true)
+    private String email;
+    
+    private String password;
+    
+    private String firstName;
+    
+    private String lastName;
+    
+    private UserRole role;
+    
+    private String avatarUrl;
+    
+    private boolean enabled;
+    
+    private LocalDateTime createdAt;
+    
+    private LocalDateTime updatedAt;
+}
